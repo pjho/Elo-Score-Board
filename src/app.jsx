0257b1b6828944b5663e941022cc24168abfc5df
Pat Ho <p@pho.sx>
2015-11-21 07:06:40 +1300

    Adds history.pushstate support to dev server for cleaner URLs.

--- src/app.jsx
+++ src/app.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { Router, Route } from 'react-router';
 import GameTable from './components/GameTable';
 import PlayerDash from './components/PlayerDash'
+import createBrowserHistory from 'history/lib/createBrowserHistory'
+
 
 const NoMatch = React.createClass({
   render() { return <div>Route not Found</div>; }
 });
 
 React.render((
-  <Router>
+  <Router history={createBrowserHistory()}>
     <Route path="/" component={GameTable}>
       <Route path="league/:leagueName" component={GameTable} />
+      <Route path="edit" component={GameTable} />
     </Route>
     <Route path="/player/:playerId" component={PlayerDash} />
     <Route path="*" component={NoMatch}/>
