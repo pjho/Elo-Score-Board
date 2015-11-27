2ecd4b515d67dfe1f0630271435f2637f220d97b
Pat Ho <p@pho.sx>
2015-11-28 06:52:04 +1300

    Adds logic to gulp file to only minify JS for prod. Adds scripts to package.json to make it easy to run gulp in dev vs prod.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gutil = require('gulp-util');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var watchify = require('watchify');
-var babelify = require('babelify')
+var babelify = require('babelify');
 var notifier = require('node-notifier');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
@@ -11,7 +11,9 @@ var minifyCss = require('gulp-minify-css');
 var watch = require('gulp-watch');
 var uglify = require('gulp-uglify');
 var buffer = require('vinyl-buffer');
-var browserSync = require('browser-sync')
+var browserSync = require('browser-sync');
+
+var prod = process.env.NODE_ENV == 'production';
 
 var notify = function(error) {
   var message = 'In: ';
@@ -31,8 +33,9 @@ var notify = function(error) {
   if(error.lineNumber) {
     message += '\nOn Line: ' + error.lineNumber;
   }
-  console.log(title);
-  console.log(message);
+
+  gutil.log(title);
+  gutil.log(message);
   notifier.notify({title: title, message: message});
 };
 
@@ -41,10 +44,10 @@ var bundler = watchify(browserify({
   entries: ['./src/app.jsx'],
   transform: [babelify],
   extensions: ['.jsx'],
-  debug: true,
+  debug: !prod,
   cache: {},
   packageCache: {},
-  fullPaths: true
+  fullPaths: !prod,
 }));
 
 function bundle() {
@@ -52,14 +55,13 @@ function bundle() {
     .on('error', notify)
     .pipe(source('main.js'))
     .pipe(buffer())
-    .pipe(uglify())
+    .pipe(prod ? uglify() : gutil.noop())
     .pipe(gulp.dest('./dist/'))
     .pipe(browserSync.stream());
 }
 
 bundler.on('update', bundle);
-gulp.task('js', function() { bundle(); });
-
+gulp.task('js', function() { return bundle(); });
 
 gulp.task('serve', function() {
     browserSync({
@@ -70,7 +72,7 @@ gulp.task('serve', function() {
 });
 
 gulp.task('sass', function () {
-  gulp.src('./sass/**/*.scss')
+  return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('style.css'))
     .pipe(minifyCss())
@@ -79,12 +81,20 @@ gulp.task('sass', function () {
 });
 
 gulp.task('copy', function () {
-  gulp.src(['index.html','fonts/**/*'],{base:'./'})
+  return gulp.src(['index.html','fonts/**/*'],{base:'./'})
     .pipe(gulp.dest('./dist/'));
 });
-
-gulp.task('default', ['js', 'copy', 'serve', 'sass', 'watch']);
 
 gulp.task('watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
+
+// use: npm run start
+gulp.task('default', ['js', 'copy', 'sass', 'serve', 'watch']);
+
+// use: npm run build
+gulp.task('build', ['js', 'copy', 'sass']);
+
+gulp.doneCallback = function (err) {
+  if( prod ) process.exit(err ? 1 : 0);
+};
